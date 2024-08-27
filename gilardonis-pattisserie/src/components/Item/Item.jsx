52ee36bf0e4e874992712e7f9f98b5424043111b
mchocho99 <martinchocho99@gmail.com>
2024-08-26 22:36:05 -0300

    Css refactor and new more element on list

--- gilardonis-pattisserie/src/components/Item/Item.jsx
+++ gilardonis-pattisserie/src/components/Item/Item.jsx
@@ -1,27 +1,39 @@
 import PropTypes from "prop-types";
 
 import MoreIcon from "../../assets/moreIcon.png";
+import MoreProduct from "../../assets/moreProducts.png";
+
+import { ITEM_TYPE } from "./types";
 
 import "./Item.css";
 
-const Item = ({ image, name, prize }) => {
+const Item = ({ image, name, prize, type }) => {
   return (
-    <div className="item">
-      <img className="more" src={MoreIcon} />
-      <img className="product" src={image} alt={name} />
-      <div className="bottom">
-        <span>
-          {name} ${prize}
-        </span>
-      </div>
-    </div>
+    <>
+      {type === ITEM_TYPE.MORE ? (
+        <div className="item">
+          <img className="more-products" src={MoreProduct} alt="More Product" />
+        </div>
+      ) : (
+        <div className="item">
+          <img className="more" src={MoreIcon} alt="More Icon" />
+          <img className="product" src={image} alt={name} />
+          <div className="bottom">
+            <span>
+              {name} ${prize}
+            </span>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
 Item.propTypes = {
   image: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  prize: PropTypes.number.isRequired,
+  name: PropTypes.string,
+  prize: PropTypes.number,
+  type: PropTypes.string,
 };
 
 export default Item;
--- gilardonis-pattisserie/src/components/List/List.jsx
+++ gilardonis-pattisserie/src/components/List/List.jsx
@@ -2,6 +2,8 @@ import PropTypes from "prop-types";
 
 import Item from "../Item/Item";
 
+import { ITEM_TYPE } from "../Item/types";
+
 import "./List.css";
 
 const List = ({ className = null, elements, title = null }) => {
@@ -10,8 +12,9 @@ const List = ({ className = null, elements, title = null }) => {
       <h2 className="title">{title}</h2>
       <div className="carroussel">
         {elements.map((element) => (
-          <Item key={element.id} {...element} />
+          <Item key={element.id} type={ITEM_TYPE.ELEMENT} {...element} />
         ))}
+        <Item key={-1} type={ITEM_TYPE.MORE} />
       </div>
     </div>
   );
